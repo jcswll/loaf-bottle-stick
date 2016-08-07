675b62eb1cc519959c4ec758e762d0bdf534af50
Josh Caswell <josh@kraken>
2016-08-06 17:50:55 -0700

    Added Xcode project and associated infrastructure, to faciliate testing.

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  LoafBottleStick
+//
+//  Created by Joshua Caswell on 8/6/16.
+//  Copyright © 2016 Josh Caswell. All rights reserved.
+//
+
+import Foundation
+
+func main(argc: Int, argv: [String]) -> Int
+{
+    return 0
+}
