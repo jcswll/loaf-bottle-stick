98cf8be939b0398e14be927237e5f37f45763844
Josh Caswell <josh@kraken>
2016-08-16 15:01:37 -0700

    Implementation and tests for `MarketPresentation`
    Exposure of name, trip, and inventory established and tested.

--- Presentations/MarketPresentation.swift
+++ Presentations/MarketPresentation.swift
@@ -1,4 +1,4 @@
-/** Present a `Market` in a way that's consumable in a CLI environment. */
+/** Present a `Market`. */
 class MarketPresentation
 {
     private var market: Market
@@ -8,63 +8,27 @@ class MarketPresentation
         self.market = market
     }
     
+    //MARK: - Fields
     /** Market name */
     var name: String 
     { 
         get { return self.market.name }
         set(name)
-        { 
+        {
             self.market.name = name
-            self.didUpdate?() 
+            self.didUpdate?()
         }
     }
     
-    var didUpdate: (() -> Void)?
-    // MARK: - Events
-    
     lazy var inventoryPresentation: InventoryPresentation = {
-        // Create 
-        let presentation = 
-               InventoryPresentation(inventory: self.market.inventory)
-        // Chain events
-        //TODO: Need shouldPurchase. Ties to Quick Entry?
-        presentation.makePurchase = { [weak self] (merch, quantity) in 
-            //TODO: Need quantity
-            self?.tripPresentation.createPurchase(ofMerch: merch, 
-                                              inQuantity: quantity)
-        }
-        presentation.merchDidChange = { [weak self] (old, new) in
-            do {
-                try self?.tripPresentation.updatePurchase(ofMerch: old, 
-                                                               to: new)
-            }
-            catch _ {
-                fatalError("Attempt to update nonexistent purchase of \(old.name)")
-            }
-        }
-        presentation.merchIsInPurchase = { [weak self] (merch) in 
-            guard let `self` = self else {
-                return false
-            }
-            return self.tripPresentation.merchIsUsed(merch)
-        }
-        presentation.didDeleteMerch = { [weak self] (merch) in
-            do {
-                try self?.tripPresentation.deletePurchase(ofMerch: merch)
-            }
-            catch _ {
-                fatalError("Attempt to delete nonexistent purchase of \(merch.name)")
-            }
-        }
-        // didCreateMerch?
-        return presentation
+        return InventoryPresentation(inventory: self.market.inventory)
     }()
     
     lazy var tripPresentation: TripPresentation = {
-        let presentation = TripPresentation(trip: self.market.trip)
-        // purchaseDidChange?
-        return presentation
+        return TripPresentation(trip: self.market.trip)
     }()
     
-    // Switch subpresentation
+    //MARK: - Events
+    /** Tell view that new values need to be read. */
+    var didUpdate: (() -> Void)?
 }
---
+++ Tests/Presentations/MarketPresentationTests.swift
@@ -0,0 +1,53 @@
+import XCTest
+@testable import LoafBottleStickKit
+
+class MarketPresentationTests : XCTestCase
+{
+    let inventory = MarketList(items: Set(Merch.dummies))
+    let trip = MarketList(items: Set(Purchase.dummies))
+    var market: Market!
+    var presentation: MarketPresentation!
+    
+    
+    override func setUp()
+    {
+        self.market = Market(name: "Abbandando's Groceria",
+                            ident: "",
+                        inventory: self.inventory,
+                             trip: self.trip)
+        self.presentation = MarketPresentation(market: self.market)
+    }
+    
+    func testPresentsName()
+    {
+        XCTAssertEqual(self.market.name, self.presentation.name)
+    }
+    
+    func testNotifiesViewOnNameChange()
+    {
+        var sentDidUpdate = false
+        self.presentation.didUpdate = { sentDidUpdate = true }
+
+        self.presentation.name = "Genco Olive Oil Co."
+        
+        XCTAssertTrue(sentDidUpdate)
+    }
+    
+    func testPresentsInventory()
+    {
+        let inventoryPresentation = self.presentation.inventoryPresentation
+        let expectedNames = Set(self.inventory.items.map { $0.name })
+        let names = Set(inventoryPresentation.subPresentations.map { $0.name })
+        
+        XCTAssertEqual(expectedNames, names)
+    }
+    
+    func testPresentsTrip()
+    {
+        let tripPresentation = self.presentation.tripPresentation
+        let expectedNames = Set(self.trip.items.map { $0.name })
+        let names = Set(tripPresentation.subPresentations.map { $0.name })
+        
+        XCTAssertEqual(expectedNames, names)
+    }
+}
